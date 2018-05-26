a3012c1051bec4bf4f342e11eb190a14c2ee89fe
59160758 <59160758@go.buu.ac.th>
2018-05-27 04:17:53 +0700

    fifth commit

--- server.js
+++ server.js
@@ -3,7 +3,22 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const app = express()
-let contacts = []
+let contacts = [{
+    name : 'Dison59160758',
+    phoneNumber : '0639596145'
+},
+{
+    name : 'Sasiprapa59160902',
+    phoneNumber : '0900164853'
+},
+{
+    name : 'Suchada59160700',
+    phoneNumber : '0863472758'
+},
+{
+    name : 'Sutarinee59160524',
+    phoneNumber : '0639596145'
+}]
 
 app.use(bodyParser.json())
 app.use(cors())
